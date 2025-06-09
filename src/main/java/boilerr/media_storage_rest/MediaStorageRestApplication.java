db4fa957f9b187c0a400aa71992e3953fa5f6c24
Boilerr <sergey.nys@gmail.com>
2025-06-09 19:56:30 +0300

    Add Spring Boot 3.5.0 template

---
+++ src/main/java/boilerr/media_storage_rest/MediaStorageRestApplication.java
@@ -0,0 +1,13 @@
+package boilerr.media_storage_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaStorageRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaStorageRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boilerr/media_storage_rest/MediaStorageRestApplicationTests.java
@@ -0,0 +1,13 @@
+package boilerr.media_storage_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaStorageRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
