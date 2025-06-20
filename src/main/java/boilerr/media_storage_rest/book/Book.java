fa593623442397e3bf76a18ae3feb10b47f5b2af
Boilerr <sergey.nys@gmail.com>
2025-06-20 19:56:25 +0300

    Add Book class

--- src/main/java/boilerr/media_storage_rest/book/Book.java
+++ src/main/java/boilerr/media_storage_rest/book/Book.java
@@ -1,4 +1,96 @@
 package boilerr.media_storage_rest.book;
 
+import jakarta.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "book")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "date")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "publicationyear")
+    private int publicationYear;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+
+    // Empty Constructor
+    public Book() {
+    }
+
+    // Constructor without id
+    public Book(Long id, String title, String author, int publicationYear, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+    }
+
+
+    // Getters and Setters for all fields
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, publicationYear, isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
